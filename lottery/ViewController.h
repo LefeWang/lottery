736398c91d03fad27f6d7494580f0bc2e3f9534a
Dreim <TeslaMac@wangdachui.local>
2016-11-03 13:36:03 +0800

    Initial Commit

---
+++ lottery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lottery
+//
+//  Created by Tesla on 2016/11/3.
+//  Copyright © 2016年 binghu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
